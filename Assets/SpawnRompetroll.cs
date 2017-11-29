1862b622f23be6115ac1fc6446bdaceb62ffbd4b
IAmDirt <jbalevik@gmail.com>
2017-11-29 14:58:03 +0100

    før rette forandrer i FacePlayer

---
+++ Assets/SpawnRompetroll.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnRompetroll : MonoBehaviour {
+
+    public GameObject Rompetroll;
+    public Vector2 SpawnDirection;
+
+    public float spawnRate;
+    float nextSpawn;
+
+    void Update () {
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            GameObject Spawn = Instantiate(Rompetroll);
+            Vector2 MyPosition = transform.position;
+            Spawn.transform.position = MyPosition + SpawnDirection;
+        }
+    }
+}
